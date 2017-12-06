e05da051a05e17204064c8b5c9a9c37077ab5298
Leslie Stevens <flylady@mail.utexas.edu>
2017-12-06 01:28:52 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -1,1 +1,18 @@
-'use strict';
+class Question {
+  constructor(content){
+    this.content = content;
+    this.save()
+  }
+  save(){
+    this.constructor._All.push(this)
+  }
+  static All() {
+    return this._All;
+  }
+  static Find(ID) {
+    return this.All().filter(function(question){
+      return question.ID === content;
+    })
+  }
+}
+Question._All = [];
